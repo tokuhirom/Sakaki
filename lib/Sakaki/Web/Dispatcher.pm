2169a3be77f23408cf86c3cff0148cbabf5f971c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-05 23:38:54 +0900

    simplified

--- lib/Sakaki/Web/Dispatcher.pm
+++ lib/Sakaki/Web/Dispatcher.pm
@@ -2,10 +2,25 @@ package Sakaki::Web::Dispatcher;
 use strict;
 use warnings;
 use utf8;
-use Amon2::Web::Dispatcher::Lite;
 use URI::Escape qw(uri_escape_utf8 uri_unescape);
 use Encode qw(decode_utf8);
 use Data::Page::Navigation;
+use Router::Simple::Sinatraish;
+
+sub dispatch {
+    my ( $class, $c ) = @_;
+
+    if ( my $p = $class->router->match( $c->request->env ) ) {
+        if (defined(my $name = $p->{name})) {
+            $name = decode_utf8(uri_unescape $name);
+            $p->{entry} = $c->repository->lookup($name);
+        }
+        return $p->{code}->( $c, $p );
+    }
+    else {
+        return $c->res_404();
+    }
+}
 
 any '/' => sub {
     my ($c) = @_;
@@ -40,42 +55,30 @@ get '/search' => sub {
 
 get '/e/:name' => sub {
     my ( $c, $args ) = @_;
-    my $name = $args->{name} // die;
-    $name = uri_unescape $name;
-    $name = decode_utf8 $name;
-    my $entry = $c->repository->lookup($name);
-    return $c->render( 'show.tt', { entry => $entry, name => $name } );
+    my $entry = $args->{entry} // die;
+    return $c->render( 'show.tt', { entry => $entry, name => $entry->name } );
 };
 get '/e/:name/log' => sub {
     my ( $c, $args ) = @_;
-    my $name = $args->{name} // die;
-    $name = uri_unescape $name;
-    $name = decode_utf8 $name;
-    my $entry = $c->repository->lookup($name);
+    my $entry = $args->{entry} // die;
     my @logs = $entry->get_log();
     return $c->render( 'log.tt', { logs => \@logs, entry => $entry } );
 };
 get '/e/:name/log/:hash' => sub {
     my ( $c, $args ) = @_;
-    my $name = $args->{name} // die;
-    $name = uri_unescape $name;
-    $name = decode_utf8 $name;
+    my $entry = $args->{entry} // die;
     my $hash = $args->{hash} // die;
-    my $entry = $c->repository->lookup($name);
     my $log = $entry->get_log_detail($hash);
     return $c->render( 'log_detail.tt', { log => $log, entry => $entry } );
 };
 any '/e/:name/edit' => sub {
     my ( $c, $args ) = @_;
-    my $name = $args->{name} // die;
-       $name = uri_unescape $name;
-       $name = decode_utf8 $name;
-    my $entry = $c->repository->lookup( $name );
+    my $entry = $args->{entry} // die;
     if ($c->req->method eq 'POST') {
         $entry->body(scalar $c->req->param('body'));
         $entry->formatter(scalar $c->req->param('formatter'));
         $entry->update();
-        return $c->redirect( "/e/" . uri_escape_utf8 $name);
+        return $c->redirect( "/e/" . uri_escape_utf8 $entry->name);
     }
 	$c->fillin_form( $entry->as_hashref );
     my $formatters =
