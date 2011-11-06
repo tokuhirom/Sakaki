939265d6105b11488f43faf1e0a331a13dba95a0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-06 20:03:36 +0900

    better dat dir detection for deployment env

--- config/deployment.pl
+++ config/deployment.pl
@@ -1,12 +1,18 @@
 use File::Spec;
 use File::Basename qw(dirname);
+use File::Path;
+
 my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
 my $dbpath;
+my $datpath;
 if ( -d '/home/dotcloud/') {
     $dbpath = "/home/dotcloud/deployment.db";
+    $datpath = '/home/dotcloud/sakaki-dat/';
 } else {
     $dbpath = File::Spec->catfile($basedir, 'db', 'deployment.db');
+    $datpath = File::Spec->catdir($basedir, 'dat');
 }
+mkpath($datpath);
 +{
     'DBI' => [
         "dbi:SQLite:dbname=$dbpath",
@@ -16,4 +22,5 @@ if ( -d '/home/dotcloud/') {
             sqlite_unicode => 1,
         }
     ],
+    root_dir => $datpath,
 };
