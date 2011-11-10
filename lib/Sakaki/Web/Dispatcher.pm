8e9d3c8e24c7cf683d8e2172348d05ffb226faa6
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-10 09:52:55 +0900

    change uri structure

--- lib/Sakaki/Web/Dispatcher.pm
+++ lib/Sakaki/Web/Dispatcher.pm
@@ -2,6 +2,7 @@ package Sakaki::Web::Dispatcher;
 use strict;
 use warnings;
 use utf8;
+use 5.10.0;
 use URI::Escape qw(uri_escape_utf8 uri_unescape);
 use Encode qw(decode_utf8);
 use Data::Page::Navigation;
@@ -12,15 +13,6 @@ sub dispatch {
 
     if ( my $p = $class->router->match( $c->request->env ) ) {
         if (defined(my $name = $p->{name})) {
-            $name = decode_utf8(uri_unescape $name);
-            my $entry = $c->repository->lookup($name);
-            if ($entry) {
-                $p->{entry} = $entry;
-            } else {
-                my $res = $c->show_error("There is no page named: " . $name);
-                $res->code(404);
-                return $res;
-            }
         }
         return $p->{code}->( $c, $p );
     }
@@ -33,9 +25,9 @@ any '/' => sub {
     my ($c) = @_;
     my $name = 'IndexPage';
     my $entry = $c->repository->lookup($name);
-    return $c->render( 'show.tt', { entry => $entry } );
+    return _handle_entry($c, $entry);
 };
-any '/recent' => sub {
+any '/_recent' => sub {
     my ($c) = @_;
     my $current_page = 0 + ( $c->req->param('page') || 1 );
     my ( $entries, $pager ) = $c->repository->get_recent(
@@ -44,7 +36,7 @@ any '/recent' => sub {
     );
     $c->render( 'recent.tt', { entries => $entries, pager => $pager } );
 };
-get '/search' => sub {
+get '/_search' => sub {
     my ($c) = @_;
 
     if (defined(my $keyword = $c->req->param('keyword'))) {
@@ -60,89 +52,7 @@ get '/search' => sub {
     }
 };
 
-get '/e/:name' => sub {
-    my ( $c, $args ) = @_;
-    my $entry = $args->{entry} // die;
-	$entry->html(); # pre-rendering
-    return $c->render( 'show.tt', { entry => $entry } );
-};
-get '/e/:name/log' => sub {
-    my ( $c, $args ) = @_;
-    my $entry = $args->{entry} // die;
-    my @logs = $entry->get_log();
-    return $c->render( 'log.tt', { logs => \@logs, entry => $entry } );
-};
-get '/e/:name/log/:hash' => sub {
-    my ( $c, $args ) = @_;
-    my $entry = $args->{entry} // die;
-    my $hash = $args->{hash} // die;
-    my $log = $entry->get_log_detail($hash);
-    return $c->render( 'log_detail.tt', { log => $log, entry => $entry } );
-};
-any '/e/:name/edit' => sub {
-    my ( $c, $args ) = @_;
-    my $entry = $args->{entry} // die;
-    if ($c->req->method eq 'POST') {
-        $entry->body(scalar $c->req->param('body'));
-        $entry->formatter(scalar $c->req->param('formatter'));
-        $entry->update();
-        return $c->redirect( $entry->path );
-    }
-	$c->fillin_form( $entry->as_hashref );
-    my $formatters =
-      [ map { +{ name => $_->moniker, pkg => $_ } }
-          Sakaki::Formatter->available_formatters() ];
-    return $c->render( 'edit.tt', { entry => $entry, formatters => $formatters } );
-};
-any '/e/:name/remove' => sub {
-    my ($c, $args) = @_;
-
-    my $entry = $args->{entry} // die;
-    if ($c->req->method eq 'POST') {
-        $entry->remove();
-        return $c->redirect( "/" );
-    }
-    return $c->render( 'remove.tt', { entry => $entry } );
-};
-
-any '/e/:name/attachments/' => sub {
-    my ($c, $args) = @_;
-
-    my $entry = $args->{entry} // die;
-	return $c->render( 'attachments/list.tt', { entry => $entry } );
-};
-post '/e/:name/attachments/add' => sub {
-    my ($c, $args) = @_;
-
-    my $entry = $args->{entry} // die;
-    my $upload = $c->req->upload('file')
-      // return $c->show_error("Please choose a upload file");
-
-	if ($upload->size > 10_000_000) {
-		return $c->show_error("Uploaded file too big");
-	}
-
-	open my $fh, '<', $upload->path;
-	$entry->add_attachment($upload->basename, $fh);
-	return $c->redirect('/e/' . $entry->name_raw . '/attachments/');
-};
-get '/e/:name/attachments/download' => sub {
-    my ($c, $args) = @_;
-
-    my $entry = $args->{entry} // die;
-	my $f = $c->req->param('f') // die;
-	my $attachment = $entry->get_attachment($f) // return $c->res_404();
-    return $c->create_response(
-        200,
-        [
-            'Content-Type'   => $attachment->mime_type,
-            'Content-Length' => -s $attachment->fullpath
-        ],
-        $attachment->openr,
-    );
-};
-
-any '/create' => sub {
+any '/_create' => sub {
     my ($c) = @_;
 
     my $name = $c->req->param('name');
@@ -170,10 +80,94 @@ any '/create' => sub {
     return $c->render( 'add.tt', { formatters => $formatters } );
 };
 
-post '/account/logout' => sub {
+post '/_logout' => sub {
     my ($c) = @_;
     $c->session->expire();
     $c->redirect('/');
 };
+
+# this hook must be last
+any '/{name:[^_].*}' => sub {
+    my ( $c, $args ) = @_;
+
+    my $name = $args->{name};
+    $name = decode_utf8(uri_unescape $name);
+    my $entry = $c->repository->lookup($name);
+    unless ($entry) {
+        my $res = $c->show_error("There is no page named: " . $name);
+        $res->code(404);
+        return $res;
+    }
+
+    return _handle_entry($c, $entry);
+};
+
+sub _handle_entry {
+    my ($c, $entry) = @_;
+
+    given ($c->req->param('mode')) {
+    when ('edit') {
+        if ($c->req->method eq 'POST') {
+            $entry->body(scalar $c->req->param('body'));
+            $entry->formatter(scalar $c->req->param('formatter'));
+            $entry->update();
+            return $c->redirect( $entry->path );
+        }
+        $c->fillin_form( $entry->as_hashref );
+        my $formatters =
+        [ map { +{ name => $_->moniker, pkg => $_ } }
+            Sakaki::Formatter->available_formatters() ];
+        return $c->render( 'edit.tt', { entry => $entry, formatters => $formatters } );
+    }
+    when ('log_detail') {
+        my $hash = $c->req->param('hash') // die;
+        my $log = $entry->get_log_detail($hash);
+        return $c->render( 'log_detail.tt', { log => $log, entry => $entry } );
+    }
+    when ('log') {
+        my @logs = $entry->get_log();
+        return $c->render( 'log.tt', { logs => \@logs, entry => $entry } );
+    }
+    when ('attachments_add') {
+        my $upload = $c->req->upload('file')
+        // return $c->show_error("Please choose a upload file");
+
+        if ($upload->size > 10_000_000) {
+            return $c->show_error("Uploaded file too big");
+        }
+
+        open my $fh, '<', $upload->path;
+        $entry->add_attachment($upload->basename, $fh);
+        return $c->redirect($c->req->uri_with({mode => 'attachments'}));
+    }
+    when ('attachments_download') {
+        my $f = $c->req->param('f') // die;
+        my $attachment = $entry->get_attachment($f) // return $c->res_404();
+        return $c->create_response(
+            200,
+            [
+                'Content-Type'   => $attachment->mime_type,
+                'Content-Length' => -s $attachment->fullpath
+            ],
+            $attachment->openr,
+        );
+    }
+    when ('attachments') {
+        return $c->render( 'attachments/list.tt', { entry => $entry } );
+    }
+    when ('remove') {
+        if ($c->req->method eq 'POST') {
+            $entry->remove();
+            return $c->redirect( "/" );
+        }
+        return $c->render( 'remove.tt', { entry => $entry } );
+    }
+    default {
+        $entry->html(); # pre-rendering
+        return $c->render( 'show.tt', { entry => $entry } );
+    }
+    }
+}
+
 
 1;
